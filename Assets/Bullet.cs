a56abe8376aa64356ba621022635b8b86ce688f4
Cam <cam+gh2@caesium.co.nz>
2022-10-08 00:32:39 +1300

    Add bullet with pushing force.

--- Assets/Bullet.cs
+++ Assets/Bullet.cs
@@ -4,10 +4,14 @@ using UnityEngine;
 
 public class Bullet : MonoBehaviour
 {
+    public Rigidbody2D body;
+    public float speed;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        Vector2 force = new Vector2(0.0f, speed);
+        body.AddForce(force);
     }
 
     // Update is called once per frame
