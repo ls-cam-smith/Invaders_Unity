c2796915cd38e67b1f366ae0d838f144e81fa1d5
Cam <cam+gh2@caesium.co.nz>
2023-01-03 22:15:21 +1300

    Add destructible barriers, make bullets go further before despawn.

---
+++ Assets/Barrier.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Barrier : MonoBehaviour
+{
+
+    public int maxHitPoints = 5;
+    public int currentHitPoints;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        currentHitPoints = maxHitPoints;
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        switch (other.tag)
+        {
+            default:
+                break;
+
+            case "PlayerBullet":
+                RegisterHit();
+                break;
+            case "EnemyBullet":
+                RegisterHit();
+                break;
+        }
+    }
+
+    void RegisterHit()
+    {
+        currentHitPoints -= 1;
+        if (currentHitPoints <= 0)
+        {
+            Destroy(this.gameObject);
+        }
+
+    }
+}
